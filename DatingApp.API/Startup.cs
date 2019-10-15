1d7d7aace4fcaf2ec113ae7d4acccbc6b9e8da86
Fredrik Weiberth <fredrik.weiberth@falun.se>
2019-10-15 13:56:48 +0200

    End of Section 4

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -40,7 +40,8 @@ namespace DatingApp.API
                 {
                     ValidateIssuerSigningKey=true,
                     IssuerSigningKey=new SymmetricSecurityKey(Encoding.ASCII
-                    .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    .GetBytes(Configuration.GetSection("AppSettings:Token").Value)
+                    ),
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
